d3938878f47fff6277586261cc5fb80f75ce609d
250T4 <kolovr4488@gmail.com>
2023-10-04 17:30:19 +0300

    first commit

--- app/src/main/java/com/example/javaoop/Cat.java
+++ app/src/main/java/com/example/javaoop/Cat.java
@@ -3,25 +3,30 @@ package com.example.javaoop;
 import android.util.Log;
 
 public class Cat extends Animal {
-    protected int age;
-    protected String name;
-    protected String breed;
-    protected String color;
+    int age;
+    String name;
+    final static  int number0fLegs =  4;
+
 
     public Cat() {
         this.name = "John Doe";
         this.age = -1;
     }
 
-    public Cat(int age, String name, String breed, String color) {
+    public Cat(int age, String name) {
         this.age = age;
         this.name = name;
-        this.breed = breed;
-        this.color = color;
     }
 
     public void talk() {
         Log.i("talk()", "Meow! I'm cat. My name is " + name +
-                ", I'm breed " + breed + " and " + color + " coloration " + age + " years old.");
+                ",and I'm "  + age + " years old.");
     }
+
+
+
+    public static String whatCatsLike () {
+        return " I like playing, jumping";
+    }
+
 }
--- app/src/main/java/com/example/javaoop/Lion.java
+++ app/src/main/java/com/example/javaoop/Lion.java
@@ -5,6 +5,10 @@ import android.util.Log;
 public class Lion extends Cat {
     public void talk() {
         Log.i("talk()", "R-r-r! I'm lion. My name is " + name +
-            ", I'm breed " + breed + " and " + color + " coloration " + age + " years old.");
+            ", and I'm " + age + " years old." + Lion.whatCatsLike());
+    }
+
+    public static String whatCatsLike () {
+        return " I'm lion and I like playing, jumping";
     }
 }
