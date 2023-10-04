6466e62e3ee175f4a03f6178d32482be5e5dc708
250T4 <kolovr4488@gmail.com>
2023-10-04 17:00:05 +0300

    first commit

--- app/src/main/java/com/example/javaoop/Puma.java
+++ app/src/main/java/com/example/javaoop/Puma.java
@@ -3,8 +3,15 @@ package com.example.javaoop;
 import android.util.Log;
 
 public class Puma extends Cat {
+    String name;
+     private int age;
+
+    public Puma() {
+        this.name = "Puma";
+        this.age = 1;
+    }
     public void talk() {
-        Log.i("talk()", "R-r-r! I'm puma. My name is " + name +
-                ", I'm breed " + breed + " and " + color + " coloration " + age + " years old.");
+        Log.i("talk()", "R-r-r! I'm a cool cat! My name is " + name +
+                ", and I'm " + age + " years old.");
     }
 }
