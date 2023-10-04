909d0500d618363395cb2d93215a9f2798d81d4a
250T4 <kolovr4488@gmail.com>
2023-10-04 15:09:25 +0300

    first commit

--- app/src/main/java/com/example/javaoop/Cat.java
+++ app/src/main/java/com/example/javaoop/Cat.java
@@ -2,22 +2,21 @@ package com.example.javaoop;
 
 import android.util.Log;
 
-public class Cat {
+public class Cat extends Animal {
     int age;
     String name;
-    String breed;
-    String color;
 
+    public Cat() {
 
-    public Cat(int age, String name, String breed, String color) {
+    }
+
+    public Cat(int age, String name) {
        this.age = age;
         this.name = name;
-        this.breed = breed;
-        this.color = color;
     }
 
     public void talk() {
         Log.i("talk()", "Meow! My name is " + name +
-                ", I'm breed " + breed + " and " + color + " coloration " + age + " years old.");
+                ", and I'm " + age + " years old.");
     }
 }
