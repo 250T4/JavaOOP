44a883ffdd7107ad832a26b065db19647dcd34a6
250T4 <kolovr4488@gmail.com>
2023-10-04 18:04:07 +0300

    first commit

--- app/src/main/java/com/example/javaoop/MainActivity.java
+++ app/src/main/java/com/example/javaoop/MainActivity.java
@@ -25,7 +25,16 @@ public class MainActivity extends AppCompatActivity {
         Cat cat3 = new Cat(8, "Murzik");
         cat3.talk();
 
-        Cat.CatMood catMood = new Cat().new CatMood();
-        Log.i("level0fMood", String.valueOf(catMood.level0fMood));
+        Cat cat4 = new Cat();
+        Cat cat5 = new Cat();
+        Cat cat6 = new Cat();
+        Cat cat7 = new Cat();
+
+
+        Log.i("count", "" + Cat.count);
+
+        Cat.CountResetter countResetter = new Cat.CountResetter();
+
+        Log.i("count", "" + Cat.count);
     }
 }
